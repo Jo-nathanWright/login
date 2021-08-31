871e250eb16eed28c9b5c74a10e9e2dbd702e6c7
Jonathan Wright <j.wright72127@gmail.com>
2021-08-31 09:15:40 -0600

    Finished Up

--- Program.cs
+++ Program.cs
@@ -13,10 +13,12 @@ namespace login
       info.AddAcount(new Info("Test", "123"));
       info.AddAcount(new Info("Secure", "h89eth4ysl09"));
       info.AddAcount(new Info("Bad", "Bad"));
-      bool loggen = true;
-      Console.BackgroundColor = ConsoleColor.Gray;
+      bool placingUser = true;
+      bool placingPass = true;
+      Console.BackgroundColor = ConsoleColor.White;
+      Console.ForegroundColor = ConsoleColor.Black;
       Console.Clear();
-      while(loggen){
+      while(placingUser){
         Console.WriteLine("Welcome To Foodie Chews, Login to Continue!");
         Console.Write("Username: ");
         string user = Console.ReadLine();
@@ -24,6 +26,20 @@ namespace login
         if(username == null){
           Console.Beep();
           Console.WriteLine("Please provide A vaild User");
+        } else {
+          placingUser = false;
+          while(placingPass){
+            Console.WriteLine("Password Required!");
+            Console.Write("Password: ");
+            string pass = Console.ReadLine();
+            Info password = info.Info.Find(p => p.Password == pass);
+            if(password == null){
+              Console.WriteLine("Invalid Password!");
+            } else {
+              placingPass = false;
+              Console.WriteLine("Welcome Back " + user + "!");
+            }
+          }
         }
       }
     }
